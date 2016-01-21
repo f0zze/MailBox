dcc1725dda857e72de1f055361a61cc98dae3419
Aigars Beinarovics <a.beinarovics@gmail.com>
2016-01-21 22:40:20 +0200

    tabs

---
+++ mailtabs/tab.js
@@ -0,0 +1,24 @@
+(function () {
+    'use strict';
+
+    angular.module('mailbox').directive('tab', Tab);
+
+    function Tab() {
+        return {
+            restrict: 'E',
+            scope: {
+                tabName: '@',
+                icon: '@'
+            },
+            templateUrl: 'mailtabs/tab.tpl.html',
+            controller: TabController,
+            controllerAs: 'tabCtrl',
+            bindToController: true
+        };
+
+        function TabController() {
+            console.dir(this);
+        }
+    }
+
+})();
